b4fff8abcd4f19460cb7e73ba6785fecfb869371
Nathan Wallace <nathan@turbot.com>
2018-09-19 05:07:54 -0400

    Add bashrc for PS1. Rename vimrc to actual .vimrc.

---
+++ .bashrc
@@ -0,0 +1,3 @@
+export CLICOLOR=1
+export LSCOLORS=ExFxBxDxCxegedabagacad
+export PS1="\[\033[33;1m\]\w\[\033[m\] $ "
--- setup.sh
+++ setup.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+# Bash
+ln -s ~/dotfiles/.bashrc       ~/.bashrc
+ln -s ~/dotfiles/.bash_profile ~/.bash_profile
+
+# Vim
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-
-ln -s ~/dotfiles/vimrc ~/.vimrc
-
+ln -s ~/dotfiles/.vimrc ~/.vimrc
 vim +PluginInstall +qall
