29fbeb686f1b987f79d25b73630a263e57892d7c
Nathan Wallace <nathan@turbot.com>
2021-12-10 12:20:13 -0500

    Add .gitconfig

--- setup.sh
+++ setup.sh
@@ -4,6 +4,9 @@
 ln -s ~/dotfiles/.bashrc       ~/.bashrc
 ln -s ~/dotfiles/.bash_profile ~/.bash_profile
 
+# Git
+ln -s ~/dotfiles/.gitconfig ~/.gitconfig
+
 # Vim
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 ln -s ~/dotfiles/.vimrc ~/.vimrc
